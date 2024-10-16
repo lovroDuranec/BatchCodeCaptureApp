33bf5730e565116d9d9fa61da080f21aeaaa5e0f
lovroDuranec <lovro.duranec@fer.unizg.hr>
2024-10-16 12:03:16 +0200

    Revert "Removed unused classes."
    This reverts commit 2e2b4bb183271ff8367df5a7d7466e6d7cffec68.

---
+++ app/src/main/java/com/example/batchcodecapture/ScanningActivity.java
@@ -0,0 +1,16 @@
+package com.example.batchcodecapture;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScanningActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanning);
+
+    }
+}
