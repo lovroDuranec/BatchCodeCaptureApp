bbfe7c9a81d4dd3a109e7cb4a371f59bfe757936
lovroDuranec <lovro.duranec@fer.unizg.hr>
2024-10-16 12:43:53 +0200

    Added camera permission. Added cameraX cameraProviderFuture implementation

--- app/src/main/java/com/example/batchcodecapture/ScanningActivity.java
+++ app/src/main/java/com/example/batchcodecapture/ScanningActivity.java
@@ -1,16 +1,41 @@
 package com.example.batchcodecapture;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.core.content.ContextCompat;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
 
 public class ScanningActivity extends AppCompatActivity {
 
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanning);
 
+
+        //cameraX
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener( () -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+            } catch (Exception e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }, ContextCompat.getMainExecutor(this));
+
+
+        //ML kit
+
     }
+
 }
