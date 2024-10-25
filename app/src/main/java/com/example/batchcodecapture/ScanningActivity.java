c6b45ab4736976df2c8c92cccd65d7fd98c0c2a6
lovroDuranec <lovro.duranec@fer.unizg.hr>
2024-10-25 12:12:40 +0200

    Added imageCapture and takephoto.

--- app/src/main/java/com/example/batchcodecapture/ScanningActivity.java
+++ app/src/main/java/com/example/batchcodecapture/ScanningActivity.java
@@ -1,25 +1,35 @@
 package com.example.batchcodecapture;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.video.internal.compat.quirk.ImageCaptureFailedWhenVideoCaptureIsBoundQuirk;
 import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.io.File;
 
 public class ScanningActivity extends AppCompatActivity {
 
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private ImageCapture imageCapture;
+    private ExecutorService cameraExecutor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanning);
 
@@ -35,7 +45,12 @@ public class ScanningActivity extends AppCompatActivity {
             }
         }, ContextCompat.getMainExecutor(this));
 
+        //cameraExecutor
+        cameraExecutor = Executors.newSingleThreadExecutor();
 
+
+        Button captureButton = findViewById(R.id.captureButton);
+        captureButton.setOnClickListener(v -> takePhoto());
         //ML kit
 
     }
@@ -47,12 +62,41 @@ public class ScanningActivity extends AppCompatActivity {
 
         CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
 
+        imageCapture = new ImageCapture.Builder().setTargetRotation(previewView.getDisplay().getRotation()).build();
+
         try {
             cameraProvider.unbindAll();
-            cameraProvider.bindToLifecycle(this, cameraSelector, preview);
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
         } catch (Exception e){
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    private void takePhoto(){
+        if (imageCapture == null) {
+            return;
+        }
+        File photoFile = new File(getExternalFilesDir(null), "photo.jpg");
+        ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(photoFile).build();
+        imageCapture.takePicture(outputFileOptions, cameraExecutor, new ImageCapture.OnImageSavedCallback() {
+            @Override
+            public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                runOnUiThread(() -> Toast.makeText(ScanningActivity.this, "Photo captured successfully!", Toast.LENGTH_LONG).show());
+            }
+
+            @Override
+            public void onError(@NonNull ImageCaptureException exception) {
+                runOnUiThread(() ->
+                        Toast.makeText(ScanningActivity.this, "Error capturing photo: " + exception.getMessage(), Toast.LENGTH_LONG).show()
+                );
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraExecutor.shutdown();
     }
 }
